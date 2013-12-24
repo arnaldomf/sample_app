c6c0dd25254cb2ba85d5b234058b1a4d11589a48
arnaldomf <arnaldomf@server.fake>
2013-12-24 11:29:40 -0200

    Add user page (section 7.2.1)

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name  "Arnaldo Mendonca"
+    email "teste@teste.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,4 +9,10 @@ describe "UserPages" do
     it { should have_content('Sign up')}
     it { should have_title('Sign Up')}
   end
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user)}
+    before {visit user_path(user)}
+    it {should have_content(user.name)}
+    it {should have_title(user.name)}
+  end
 end
