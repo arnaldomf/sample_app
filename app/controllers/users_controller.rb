0f6aa0159c531c738146727bb1741e379cf4007b
arnaldomf <arnaldomf@server.fake>
2014-01-02 11:08:53 -0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update, :index]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
 
   def index
     @users = User.paginate(page: params[:page])
@@ -54,6 +61,9 @@ class UsersController < ApplicationController
 #      redirect_to root_url if current_user.id != params[:id]
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?      
     end
 
 end
